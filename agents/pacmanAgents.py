dcd7b6734d6dbd626f695c6c86f1250b037263ba
Jolene115 <162278473+Jolene115@users.noreply.github.com>
2025-12-03 19:31:50 +1100

    Create pacmanAgents.py

---
+++ agents/pacmanAgents.py
@@ -0,0 +1,24 @@
+# pacmanAgents.py
+
+
+import random
+
+import game
+import util
+from game import Agent
+from pacman import Directions
+
+
+class LeftTurnAgent(game.Agent):
+    "An agent that turns left at every opportunity"
+
+    def getAction(self, state):
+        legal = state.getLegalPacmanActions()
+        current = state.getPacmanState().configuration.direction
+        if current == Directions.STOP: current = Directions.NORTH
+        left = Directions.LEFT[current]
+        if left in legal: return left
+        if current in legal: return current
+        if Directions.RIGHT[current] in legal: return Directions.RIGHT[current]
+        if Directions.LEFT[left] in legal: return Directions.LEFT[left]
+        return Directions.STOP
