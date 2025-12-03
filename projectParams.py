ab9bd15ffc6a7fe348bd4465623d1e885b38afba
Jolene115 <162278473+Jolene115@users.noreply.github.com>
2025-12-03 19:25:44 +1100

    Create projectParams.py

---
+++ projectParams.py
@@ -0,0 +1,7 @@
+# projectParams.py
+
+
+STUDENT_CODE_DEFAULT = 'analysis.py,qlearningAgents.py,PacmanValueIterationAgent.py'
+PROJECT_TEST_CLASSES = 'reinforcementTestClasses.py'
+PROJECT_NAME = 'Project 3: Reinforcement learning'
+BONUS_PIC = False
