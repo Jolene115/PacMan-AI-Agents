57c06c150949e67cb6214e4190b8e4c558cad8a7
Jolene115 <162278473+Jolene115@users.noreply.github.com>
2025-12-03 19:28:47 +1100

    Create ghostAgents.py

---
+++ agents/ghostAgents.py
@@ -0,0 +1,25 @@
+# ghostAgents.py
+
+
+import random
+
+import util
+from game import Actions, Agent, Directions
+from util import manhattanDistance
+
+
+class GhostAgent( Agent ):
+    def __init__( self, index ):
+        self.index = index
+
+    def getAction( self, state ):
+        dist = self.getDistribution(state)
+        if len(dist) == 0:
+            return Directions.STOP
+        else:
+            return util.chooseFromDistribution( dist )
+
+    def getDistribution(self, state):
+        "Returns a Counter encoding a distribution over actions from the provided state."
+        util.raiseNotDefined()
+
